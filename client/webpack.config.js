a12c6a3aa7bf5086b12c293ee4da41cfa7ec1116
Brandon <beegee54321@gmail.com>
2024-04-09 00:57:29 -0700

    configured plugins for service worker/manifest file added loaders/babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,45 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({ template: './index.html', 
+        title: 'Webpack Plugin', 
+    }),
+    
+      new InjectManifest({ swSrc: './src-sw.js', 
+        swDest: 'src-sw.js', 
+    }),
+
+      new WebpackPwaManifest({ fingerprints: false, inject: true,
+        name: 'Text Editor', short_name: 'TE', 
+        description: 'Text Editor', 
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [{ src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+        },],
+    }),
     ],
 
     module: {
-      rules: [
-        
+      rules: [{ 
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test:/\.mjs$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',],
+            },
+        },
+      },
       ],
     },
   };
